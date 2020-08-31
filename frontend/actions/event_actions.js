f611d68cb069684d5c013c6c1a068daf261c29fb
Ben Hafner <brhafner@gmail.com>
2020-08-31 14:42:42 -0700

    add event actions

---
+++ frontend/actions/event_actions.js
@@ -0,0 +1,66 @@
+import * as EventApiUtil from '../util/event_api_util';
+
+export const RECEIVE_ALL_EVENTS = 'RECEIVE_ALL_EVENTS';
+export const RECEIVE_EVENT = 'RECEIVE_EVENT';
+export const DELETE_EVENT = 'DELETE_EVENT';
+export const RECEIVE_EVENT_ERRORS = 'RECEIVE_EVENT_ERRORS';
+
+const receieveAllEvents = (events) => ({
+    type: RECEIVE_ALL_EVENTS,
+    events
+})
+
+const receiveEvent = (event) => ({
+    type: RECEIVE_EVENT,
+    event
+})
+
+const removeEvent = (eventId) => ({
+    type: DELETE_EVENT,
+    eventId
+})
+
+const receiveErrors = (errors) => ({
+    type: RECEIVE_EVENT_ERRORS,
+    errors
+})
+
+export const requestAllEvents = () => dispatch => (
+    EventApiUtil.fetchEvents()
+        .then(
+            events => dispatch(receieveAllEvents(events)),
+            errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+);
+
+export const requestEvent = (eventId) => dispatch => (
+    EventApiUtil.fetchEvent(eventId)
+        .then(
+            event => dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+);
+
+export const createEvent = (event) => dispatch => (
+    EventApiUtil.createEvent(event)
+        .then(
+            event => dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+);
+
+export const updateEvent = (event) => dispatch => (
+    EventApiUtil.updateEvent(event)
+        .then(
+            event => dispatch(receiveEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+);
+
+export const deleteEvent = (event) => dispatch => (
+    EventApiUtil.deleteEvent(event)
+        .then(
+            event => dispatch(removeEvent(event)),
+            errors => dispatch(receiveErrors(errors.responseJSON))
+        )
+);
