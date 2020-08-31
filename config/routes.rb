aaa1de0b9f7fba691f56a88dcc530a76d24a3234
Ben Hafner <brhafner@gmail.com>
2020-08-31 14:26:59 -0700

    add events api util file

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
-    resources :groups
-    resources :events
-    resources :memberships, only: [:create, :destroy]
-    resources :attendances, only: [:create, :destroy]
-
+    resources :groups do
+      resources :events, only: [:index]
+      resources :memberships, only: [:create, :destroy]
+    end
+    resources :events do
+      resources :attendances, only: [:create, :destroy]
+    end
+    
   end
 
   root to: 'static_pages#root'
